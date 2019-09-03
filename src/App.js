48d2d3a2a888185babbbf59781aa73b778875426
mradzwilla <mradzwilla@gmail.com>
2019-09-03 00:20:58 -0400

    Followed tutorial to set up barebones Spotify Web Player with SDK

--- src/App.js
+++ src/App.js
@@ -1,26 +1,176 @@
 import React from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      //token: "",
+      token: "BQCiX-uRMFnrAzDlfSQ_9-38LypHeVTWy1V8-cgStDda5uJ7Pa97CQw-6LyxpIDD-QlMxuU5ymoAfbknElYNB079Ii94cpSdcavpbAvUMb4RlT9e9aPd97aMmOuxs-HSMRf4FnYw8569BJI-cCI5RQN3r1lf_tJGilWDUA",
+      deviceId: "",
+      loggedIn: false,
+      error: "",
+      trackName: "Track Name",
+      artistName: "Artist Name",
+      albumName: "Album Name",
+      playing: false,
+      position: 0,
+      duration: 0,
+    };
+    this.playerCheckInterval = null;
+  }
+  handleLogin() {
+    if (this.state.token !== "") {
+      this.setState({ loggedIn: true });
+      // check every second for the player.
+      this.playerCheckInterval = setInterval(() => this.checkForPlayer(), 1000);
+    }
+  }
+  checkForPlayer() {
+    const { token } = this.state;
+
+    if (window.Spotify !== null) {
+      // cancel the interval
+      clearInterval(this.playerCheckInterval);
+
+      this.player = new window.Spotify.Player({
+        name: "Mike's Spotify Player",
+        getOAuthToken: cb => { cb(token); },
+      });
+      this.createEventHandlers();
+
+      // finally, connect!
+      this.player.connect();
+    }
+  }
+  createEventHandlers() {
+    this.player.on('initialization_error', e => { console.error(e); });
+    this.player.on('authentication_error', e => {
+      console.error(e);
+      this.setState({ loggedIn: false });
+    });
+    this.player.on('account_error', e => { console.error(e); });
+    this.player.on('playback_error', e => { console.error(e); });
+
+    // Playback status updates
+    this.player.on('player_state_changed', state => this.onStateChanged(state));
+
+    // Ready
+    this.player.on('ready', async data => {
+      let { device_id } = data;
+      console.log("Let the music play on!");
+      await this.setState({ deviceId: device_id });
+      this.transferPlaybackHere();
+    });
+
+  }
+  onStateChanged(state) {
+    // if we're no longer listening to music, we'll get a null state.
+    if (state !== null) {
+      const {
+        current_track: currentTrack,
+        position,
+        duration,
+      } = state.track_window;
+      const trackName = currentTrack.name;
+      const albumName = currentTrack.album.name;
+      const artistName = currentTrack.artists
+        .map(artist => artist.name)
+        .join(", ");
+      const playing = !state.paused;
+      this.setState({
+        position,
+        duration,
+        trackName,
+        albumName,
+        artistName,
+        playing
+      });
+    }
+  }
+  onPrevClick() {
+  this.player.previousTrack();
+  }
+
+  onPlayClick() {
+    this.player.togglePlay();
+  }
+
+  onNextClick() {
+    this.player.nextTrack();
+  }
+
+  transferPlaybackHere() {
+    const { deviceId, token } = this.state;
+    fetch("https://api.spotify.com/v1/me/player", {
+      method: "PUT",
+      headers: {
+        authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        "device_ids": [ deviceId ],
+        "play": true,
+      }),
+    });
+    }
+  render() {
+    const {
+      token,
+      loggedIn,
+      artistName,
+      trackName,
+      albumName,
+      error,
+      position,
+      duration,
+      playing,
+    } = this.state;
+
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>Now Playing</h2>
+          <p>A Spotify Web Playback API Demo.</p>
+          <p>Artist: {artistName}</p>
+          <p>Track: {trackName}</p>
+          <p>Album: {albumName}</p>
+          <p>
+            <button onClick={() => this.onPrevClick()}>Previous</button>
+            <button onClick={() => this.onPlayClick()}>{playing ? "Pause" : "Play"}</button>
+            <button onClick={() => this.onNextClick()}>Next</button>
+          </p>
+        </div>
+
+        {error && <p>Error: {error}</p>}
+
+        {loggedIn ?
+        (<div>
+          <p>Artist: {artistName}</p>
+          <p>Track: {trackName}</p>
+          <p>Album: {albumName}</p>
+        </div>)
+        :
+        (<div>
+          <p className="App-intro">
+            Enter your Spotify access token. Get it from{" "}
+            <a href="https://beta.developer.spotify.com/documentation/web-playback-sdk/quick-start/#authenticating-with-spotify">
+              here
+            </a>.
+          </p>
+          <p>
+            <input type="text" value={token} onChange={e => this.setState({ token: e.target.value })} />
+          </p>
+          <p>
+            <button onClick={() => this.handleLogin()}>Go</button>
+          </p>
+        </div>)
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
